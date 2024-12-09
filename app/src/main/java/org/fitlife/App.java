1e75cac98b3ca2aed922e091d08f2d58fe67df09
ltexocotitlap <ltexocotitlap2300@alumno.ipn.mx>
2024-12-09 14:56:07 -0600

    feat: Add dashboard with client management and navigation

--- app/src/main/java/org/fitlife/App.java
+++ app/src/main/java/org/fitlife/App.java
@@ -54,8 +54,7 @@ public class App extends Application {
         button.setOnAction(e -> {
             try {
                 conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/fitlife", txtUser.getText(), txtPass.getText());
-                System.out.println("Connected to database");
-                //stage.setScene(new Scene(createDashboard(stage), 800, 600));
+                stage.setScene(new Scene(createDashboard(stage), 800, 600));
             } catch (SQLException ex) {
                 ex.printStackTrace();
             }
@@ -66,6 +65,81 @@ public class App extends Application {
         root.getChildren().add(vbox);
         return root;
     }
+
+    private Pane createDashboard(Stage stage) {
+        Pane root = new Pane();
+        root.setPrefSize(800, 600);
+
+        Label label = new Label("Dashboard");
+        Button addClient = new Button("Add Client");
+        Button addTrainer = new Button("Add Trainer");
+        Button addGround = new Button("Add Ground");
+        Button addCourse = new Button("Add Course");
+
+        Button button = new Button("Logout");
+        button.setOnAction(e -> {
+            stage.setScene(new Scene(createContent(stage), 800, 600));
+        });
+
+        addClient.setOnAction(e -> {
+            stage.setScene(new Scene(addClient(stage), 800, 600));
+        });
+
+        VBox vbox = new VBox(label, addClient, addTrainer, addGround, addCourse, button);
+        vbox.setSpacing(10);
+
+        root.getChildren().add(vbox);
+
+        return root;
+    }
+
+    private Pane addClient(Stage stage) {
+        Pane root = new Pane();
+        root.setPrefSize(800, 600);
+
+        Label label = new Label("Add Client");
+        Label lblName = new Label("Name:");
+        TextField txtName = new TextField();
+        Label lblEmail = new Label("Email:");
+        TextField txtEmail = new TextField();
+        Label lblPhone = new Label("Phone:");
+        TextField txtPhone = new TextField();
+        Label lblAddress = new Label("Address:");
+        TextField txtAddress = new TextField();
+        Label lblDOB = new Label("Date of Birth:");
+        TextField txtDOB = new TextField();
+
+        Button addClient = new Button("Add Client");
+
+        addClient.setOnAction(e -> {
+            // only with name and last name
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            String sql = "INSERT INTO client(name, lastname) VALUES (?, ?)";
+            try {
+                ps = conn.prepareStatement(sql);
+                ps.setString(1, txtName.getText());
+                ps.setString(2, txtEmail.getText());
+                ps.executeUpdate();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        Button button = new Button("Logout");
+        button.setOnAction(e -> {
+            stage.setScene(new Scene(createContent(stage), 800, 600));
+            // Go back to login screen
+        });
+
+        VBox vbox = new VBox(label, lblName, txtName, lblEmail, txtEmail, lblPhone, txtPhone, lblAddress, txtAddress, lblDOB, txtDOB, addClient, button);
+        vbox.setSpacing(10);
+
+        root.getChildren().add(vbox);
+
+        return root;
+    }
+
 
     public static void main(String[] args) {
         launch(args);
